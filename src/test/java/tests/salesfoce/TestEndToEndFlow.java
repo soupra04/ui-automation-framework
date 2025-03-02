8a24f4b8cc2f720b0f14bb0a105103d55d19f551
acer <acer@LAPTOP-G457OLQ6>
2025-03-02 22:49:31 +0530

    commit

--- src/test/java/tests/salesfoce/TestEndToEndFlow.java
+++ src/test/java/tests/salesfoce/TestEndToEndFlow.java
@@ -4,6 +4,10 @@ import org.testng.annotations.Test;
 
 import base.CommonToAllTest;
 import driver.DriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import pages.pageObjectModal.salesforce.DashboardPage_Salesforce_POM;
 import pages.pageObjectModal.salesforce.EndToEndFlow;
 import pages.pageObjectModal.salesforce.LoginPage_Salesforce_POM;
@@ -11,7 +15,10 @@ import utils.PropertiesReader;
 
 public class TestEndToEndFlow extends CommonToAllTest {
 	
-	@Test
+	@Test (priority=3)
+	@Description ( "Testing from End to End flow")
+	@Severity(SeverityLevel.CRITICAL)
+	 @Owner("QA_Team")
 	public void endToEnd() {
 		LoginPage_Salesforce_POM loginPageSalesforcePom = new LoginPage_Salesforce_POM(DriverManager.getDriver());
 
--- src/test/java/tests/salesfoce/TestImportBoM.java
+++ src/test/java/tests/salesfoce/TestImportBoM.java
@@ -5,6 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import base.CommonToAllTest;
 import driver.DriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import pages.pageObjectModal.salesforce.DashboardPage_Salesforce_POM;
 import pages.pageObjectModal.salesforce.ImportBoMs;
 import pages.pageObjectModal.salesforce.LoginPage_Salesforce_POM;
@@ -13,6 +17,9 @@ import utils.PropertiesReader;
 public class TestImportBoM extends CommonToAllTest {
 
 	@Test (priority=1)
+	@Description ("Testing Import of Disti BoM")
+	@Severity(SeverityLevel.NORMAL)
+	 @Owner("QA_Team")
 	public void testImportBom() {
 		
 		LoginPage_Salesforce_POM loginPageSalesforcePom = new LoginPage_Salesforce_POM(DriverManager.getDriver());
@@ -29,7 +36,9 @@ public class TestImportBoM extends CommonToAllTest {
 		 
 	}
 	@Test (priority=2)
-	
+	@Severity(SeverityLevel.CRITICAL)
+	@Description ("Testing Import of Deal Reg BoM")
+	 @Owner("QA_Team")
 	public void testImportDeal() {
 		
 		LoginPage_Salesforce_POM loginPageSalesforcePom = new LoginPage_Salesforce_POM(DriverManager.getDriver());
